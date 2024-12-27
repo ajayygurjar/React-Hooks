9d25990201d890a4f3ba2ef097cb8b2749b05fab
ajayygurjar <ajay1651@gmail.com>
2024-12-27 18:19:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInStatus = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInStatus === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
+
+  return (
+    <>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </>
+  );
+}
+
+export default App;
