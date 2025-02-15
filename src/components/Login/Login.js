a5dca10e65b4e72a17dc00a2b88eac5e722e1dd2
ajayygurjar <ajay1651@gmail.com>
2025-02-16 02:10:06 +0530

    useImperativeHandle Hook & forwardRef Method for focusing on input fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
+import Input from '../UI/Input/Input'
 
 
 
@@ -45,7 +46,7 @@ const Login = () => {
     value: "",
     isValid: null,
   });
-
+/*
   // useRef for focusing on email during first render
 
   const emailRef=useRef(null);
@@ -54,7 +55,7 @@ const Login = () => {
   useEffect(()=>{
     emailRef.current.focus();
   },[])
-
+*/
 
 
   
@@ -62,20 +63,25 @@ const Login = () => {
 //useContext
   const ctx=useContext(AuthContext)
 
-  //Destructuring from object the values of isValid form state object;
+  
   const {isValid:emailIsValid}=emailState;
   const {isValid:passwordIsValid}=passwordState;
 
 
+  const emailInputRef=useRef();
+  const passwordInputRef=useRef();
+
+
+
   useEffect(()=>{
     const timer=setTimeout(()=>{
-     // console.log('Running')
+     
       setFormIsValid(passwordIsValid&& emailIsValid)
-      // also could have have used passwordState.isvalid and emailState.isValid;
+     
     },500)
 
     return ()=>{
-      //console.log('CleanUp')
+     
       clearTimeout(timer)
     }
   },[emailIsValid,passwordIsValid])
@@ -84,16 +90,14 @@ const Login = () => {
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", payload: event.target.value });
 
-    // Validate form after email change
-    //setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
+    
   };
 
   // Password input change handler
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "USER_PASS", payload: event.target.value });
 
-    // Validate form after password change
-    //setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    
   };
 
   // Email input blur handler
@@ -108,44 +112,46 @@ const Login = () => {
 
   // Form submission handler
   const submitHandler = (event) => {
+
     event.preventDefault();
-    ctx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid){
+      ctx.onLogin(emailState.value, passwordState.value);  
+    }
+    else if(!emailIsValid){
+      emailInputRef.current.focus()
+
+    }else{
+      passwordInputRef.current.focus()
+
+    }
+    
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            ref={emailRef}
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input  
+        ref={emailInputRef}
+        id='email' 
+        type='email' 
+        label='E-mail'
+        value={emailState.value} 
+        onChange={emailChangeHandler}
+         onBlur={validateEmailHandler}
+         isValid={emailIsValid}
+         />
+        <Input  
+        ref={passwordInputRef}
+        id='password' 
+        type='password' 
+        label='Password'
+        value={passwordState.value} 
+        onChange={passwordChangeHandler}
+         onBlur={validatePasswordHandler}
+         isValid={passwordIsValid}
+         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
